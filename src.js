6e4b5847ae6f608434b58a268dc9aeaccc6075c6
kakiii <43228828+kakiii@users.noreply.github.com>
2022-01-26 22:49:36 +0000

    Update src.js

--- src.js
+++ src.js
@@ -2,10 +2,11 @@
 // @name         Bilibili Do Not Track
 // @namespace    https://github.com
 // @version      0.1
-// @description  try to take over the world!
+// @description  delete digital fingerprint of bilibili
 // @author       Kaki Wang
 // @match        https://www.bilibili.com/video/*?*
 // @icon         https://www.google.com/s2/favicons?domain=bilibili.com
+// @include      https?:\/\/www.bilibili.com
 // @grant        none
 // ==/UserScript==
 
