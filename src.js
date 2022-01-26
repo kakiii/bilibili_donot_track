cf9d7c067a535a36c81eeec59afe09e115c1ca9f
kakiii <43228828+kakiii@users.noreply.github.com>
2022-01-26 22:29:01 +0000

    Add files via upload

---
+++ src.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       Kaki Wang
+// @match        https://www.bilibili.com/video/*?*
+// @icon         https://www.google.com/s2/favicons?domain=bilibili.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    //alert(window.location.href.split("?")[0]);
+    window.location=window.location.href.split("?")[0];
+})();
