c878aa6268753f3931bd7d942d8e077e0cbbee17
kakiii <43228828+kakiii@users.noreply.github.com>
2022-02-01 17:23:36 +0000

    Update src.js

--- src.js
+++ src.js
@@ -8,6 +8,7 @@
 // @icon         https://www.google.com/s2/favicons?domain=bilibili.com
 // @include      https?:\/\/www.bilibili.com
 // @grant        none
+// @updateURL    https://raw.githubusercontent.com/kakiii/bilibili_donot_track/main/src.js
 // ==/UserScript==
 
 (function() {
@@ -16,4 +17,5 @@
     // Your code here...
     //alert(window.location.href.split("?")[0]);
     window.location=window.location.href.split("?")[0];
+    alert("Removed!");
 })();
