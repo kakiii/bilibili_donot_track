a85ad3a5765fa8c52048f642d5ec2f6160916cf1
kakiii <43228828+kakiii@users.noreply.github.com>
2022-04-01 12:56:49 +0100

    update logic error

--- src.user.js
+++ src.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         Bilibili Do Not Track
 // @namespace    https://github.com
-// @version      0.1.3
+// @version      0.1.4
 // @description  delete digital fingerprint of bilibili
 // @author       Kaki Wang
 // @match        https://*.bilibili.com/*?*
 // @icon         https://www.google.com/s2/favicons?domain=bilibili.com
 // @include      https?:\/\/www.bilibili.com
 // @grant        none
-// @updateURL    https://raw.githubusercontent.com/kakiii/bilibili_donot_track/main/src.user.js
+// @downloadURL    https://raw.githubusercontent.com/kakiii/bilibili_donot_track/main/src.user.js
 // ==/UserScript==
 
 (function() {
@@ -20,7 +20,7 @@
     const sub_domain = url.split("//")[1].split(".")[0];
     // alert(sub_domain);
     //do nothing for domain of https://live.bilibili.com/p/eden/area-tags
-    if (sub_domain!=="search" || sub_domain!=="live"){
+    if (sub_domain!=="search" && sub_domain!=="live"){
         window.location=url.split("?")[0];
     }else if(url.includes("&")){
         window.location=url.split("&")[0];
